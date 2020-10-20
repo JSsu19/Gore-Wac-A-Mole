ed9f2afcd0230914fbe32143d7e50b252e9b41d6
Loothar03 <henrydalin03@gmail.com>
2020-10-20 14:01:35 +0200

    Main Meny klar!

--- Assets/Scripts/Scenemanager.cs
+++ Assets/Scripts/Scenemanager.cs
@@ -7,6 +7,13 @@ public class Scenemanager: MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Detta gör så att den laddar nästa scen i sceneindex
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit"); //Eftersom detta inte fungerar i unity editor så kollar man så det fungerar i debug
+
+        Application.Quit(); //Säger till Application att stänga ner
     }
 }
