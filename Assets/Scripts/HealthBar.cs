a7ea48ed365a3940acd422bae0dc1927ef53c9e4
JSsg19 <60390385+JSsg19@users.noreply.github.com>
2020-10-21 09:30:37 +0200

    Healthbar 1.3 (success)
    Fixade healthbaren helt och hållet, man har nu 3 liv och dör efter de är slut. Gradient fungerar också nu.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,20 +6,30 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField]
-    private int maxHealth;
+    public int maxHealth;
 
     public Slider slider;
     public Image fill;
+    public Gradient gradient;
 
-    private int currentHealth;
-
+    public int currentHealth;
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
+    }
     public void SetMaxHealth(int maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
+
+        fill.color = gradient.Evaluate(1f);
     }
-    public void SetHealth(int currentHealth)
+    public void TakeDamage(int damage)
     {
+        currentHealth -= damage;
         slider.value = currentHealth;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
