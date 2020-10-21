ef1324f10284a2ae4f4d6fe9ff3b7b95e23b8ade
MathiasCa <56590320+MathiasCa@users.noreply.github.com>
2020-10-21 09:58:52 +0200

    Hammare
    Script hammare

---
+++ Assets/Sqripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform objectToMove;
+    public Transform target;
+    Camera cam;
+    void Update()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+
+        objectToMove.position = target.position;
+    }
+}
